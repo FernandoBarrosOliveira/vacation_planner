abbf8aaf554033830c14ce1540070a94cffde322
Fernando Barros <fernandobarros@fcmsistemas.com>
2018-02-17 14:41:37 -0300

    Initial Commit

---
+++ vacation planer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vacation planer
+//
+//  Created by Fernando Barros on 17/02/2018.
+//  Copyright © 2018 Fernando Barros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
