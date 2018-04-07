73b0fb1bdb5edf792e187c385aa1440c545ea5bd
fa7labs <fa7labs@fa7-l40m09.local>
2018-04-07 16:30:00 -0300

    Final do Layout

--- vacation planer/ViewController.swift
+++ vacation planer/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayHoliday.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resultLabel", for: indexPath)
+        
+        let myResult = self.arrayHoliday[indexPath.row]
+        
+        cell.textLabel?.text = "\(myResult.dateInit!) --> \(myResult.dateEnd!)"
+        
+        
+        return cell
+    }
+    
+    var arrayHoliday = [HolidayResult]()
     
     @IBOutlet weak var stepperDays: UIStepper!
     @IBOutlet weak var txtDays: UITextField!
@@ -18,30 +33,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnCity: UIButton!
     @IBOutlet weak var btnWeather: UIButton!
     
-    var days = 0
-    var city : Cities?
-    var weathers = [Weather]()
+    @IBOutlet weak var tvResult: UITableView!
+    @IBOutlet weak var labelResult: UILabel!
+    var forecasts = [Forecast]()
     
-    var cities = [Cities]()
+    @IBAction func SearchResults(_ sender: Any) {
+        
+        WeatherService.getForecast(codeCity: (SearchTravel.city?.woeid)!, year: "2018") {(results:[Forecast]) in
+            
+            self.forecasts = results
+            
+            self.arrayHoliday = ForecastBO.calculteBestPeriod(forcasts: self.forecasts, weathersChoice: SearchTravel.weather, daysOfHoliday: SearchTravel.days!)
+            
+            DispatchQueue.main.async {
+                
+                //let text = arrayHoliday.map{"\($0.dateInit!) - \($0.dateEnd!)"}.joined(separator: "\n")
+                
+                self.labelResult.text = "Resultados: \(self.arrayHoliday.count)"
+                self.tvResult.reloadData()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        WeatherService.getCities() { (results:[Cities]) in
-
-            self.cities = results
-        }
+  
+        clearSearch()
         updateSearch()
+        
+        self.tvResult.delegate = self
+        self.tvResult.dataSource = self
         
     }
 
     @IBAction func DaysChange(_ sender: Any) {
         
         self.txtDays.text = Int((sender as! UIStepper).value).description
+        SearchTravel.days = Int((sender as! UIStepper).value)
+        updateSearch()
     }
     
     override func didMove(toParentViewController parent: UIViewController?) {
-        //txtCity.text = SearchTravel.city?.district
+        
         updateSearch()
     }
    
@@ -59,13 +92,12 @@ class ViewController: UIViewController {
         self.txtDays.text = SearchTravel.days?.description
     }
     
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     @IBAction func UpdateDays(_ sender: Any) {
         
-        self.days = Int((sender as! UITextField).text!)!
+       // self.days = Int((sender as! UITextField).text!)!
     }
 }
 
